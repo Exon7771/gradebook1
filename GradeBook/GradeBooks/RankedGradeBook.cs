114ed92b333ee7a8547251ac46e4e4f0d43f7236
Exon7771 <exon54321@gmail.com>
2022-04-03 14:12:01 +0200

    RankedGradeBook_GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,26 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var x = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[x - 1])
+                return 'A';
+            else if (averageGrade >= grades[(x * 2) - 1])
+                return 'B';
+            else if (averageGrade >= grades[(x * 3) - 1])
+                return 'C';
+            else if (averageGrade >= grades[(x * 4) - 1])
+                return 'D';
+            return 'F';
         }
     }
 }
